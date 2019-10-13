9aa1eedf0033382b7c84f8c10b81d97ac95292c6
Ivan Savov <ivan.savov@gmail.com>
2019-10-13 19:31:26 -0400

    updated reset_and_load_fixture.sh

--- reset_and_load_fixture.sh
+++ reset_and_load_fixture.sh
@@ -1,11 +1,13 @@
-echo "yes" | ./alignmentpro/manage.py reset_db
+./alignmentpro/manage.py reset_db
 #
 echo "migrating..."
 ./alignmentpro/manage.py migrate
 #
 echo "Loading application constants..."
 ./alignmentpro/manage.py loaddata alignmentpro/alignmentapp/fixtures/paremeters.json
+./alignmentpro/manage.py loaddata alignmentpro/alignmentapp/fixtures/subject_areas.json
 ./alignmentpro/manage.py loaddata alignmentpro/alignmentapp/fixtures/admin_user.json
+
 #
 echo "Loading data fixtures..."
 ./alignmentpro/manage.py loaddata imports/curriculumdocuments/CCSSM.json
